980b406515b8c57ab89f5f77d53b09a3f28b97cc
Tamil Selvan S <tamilselvanyes@gmail.com>
2022-05-10 21:54:43 +0530

    Task completion

--- helper.js
+++ helper.js
@@ -62,3 +62,37 @@ export function createroom(room) {
 export function bookroom(room) {
   return client.db("b30wd").collection("bookings").insertOne(room);
 }
+
+export function getRoomById(id) {
+  return client.db("b30wd").collection("hallrooms").findOne({ room_id: id });
+}
+
+export function getBookingsForRoom(id) {
+  return client
+    .db("b30wd")
+    .collection("bookings")
+    .find({ room_id: id })
+    .toArray();
+}
+
+export function getAllRoomDataWithBookings() {
+  return client
+    .db("b30wd")
+    .collection("hallrooms")
+    .aggregate([
+      {
+        $lookup: {
+          from: "bookings",
+          localField: "room_id",
+          foreignField: "room_id",
+          as: "bookings",
+        },
+      },
+      { $project: { amenties: 0, _id: 0 } },
+    ])
+    .toArray();
+}
+
+export function getBookingData() {
+  return client.db("b30wd").collection("bookings").find({}).toArray();
+}
--- index.js
+++ index.js
@@ -15,7 +15,12 @@ import {
   getMentorByStudentName,
   createroom,
   bookroom,
+  getRoomById,
+  getBookingsForRoom,
+  getAllRoomDataWithBookings,
+  getBookingData,
 } from "./helper.js";
+
 const app = express();
 dotenv.config();
 
@@ -198,7 +203,6 @@ app.post("/assignmentor", async (req, res) => {
       message: "Student not exist please create before you assign a mentor ",
     });
   } else {
-    console.log(student.mentor);
     if (student.mentor !== undefined) {
       return res.send({
         message:
@@ -241,21 +245,124 @@ app.post("/assignmentor", async (req, res) => {
 //HALL BOOKING  INFORMATION STARTS HERE
 
 //API to create room
+//Eg: body of API  /createroom
+// {"room_id":"7",
+// "no_of_seats": "10",
+// "amenties": { "kitchen": "true",
+//                "TV": "true",
+//                "hair_dyer": "true",
+//                "ac": "true",
+//                "essential_kit": "true"
+//             },
+// "price_per_hour": "2000"
+// }
 
 app.post("/createroom", async (req, res) => {
   const data = req.body;
+  //before adding a room we have to check whether the same room is already present or not
+  let already_existing = await getRoomById(data.room_id);
+  if (already_existing) {
+    res.send({ message: "Room already exists" });
+    return;
+  }
   const response = await createroom(data);
+
   res.send(response);
 });
 
+//API to book room.
+// Eg: body of booking request  /bookroom
+// {
+//   "room_id":"1",
+//   "customer_name": "Ronaldo",
+//   "date": "05/10/2022",
+//   "start_time": "10:00",
+//   "end_time": "12:00"
+//   }
+
 app.post("/bookroom", async (req, res) => {
   const data = req.body;
+
+  //first check if the room exists or not
+  const room = await getRoomById(data.room_id);
+  if (room === null) {
+    res.send({ message: "Room not found" });
+    return;
+  }
+  //before booking a room we should check whether the room is available for the particular time.
+  //  1. check the room id, if there is any booking for the rooms
+
+  const bookings_of_room = await getBookingsForRoom(data.room_id);
+
+  if (bookings_of_room != null) {
+    //  2. check the date
+    for (let i = 0; i < bookings_of_room.length; i++) {
+      if (bookings_of_room[i].date === data.date) {
+        //  3. check the time, check whether the start time lies between the start time and
+        // the end time of the previous bookings
+
+        if (IsTimeoverlap()) {
+          res.send({ message: "Bookings for the Respective time is closed " });
+          return;
+        }
+
+        function gethours(date) {
+          return date.getHours(); // gives the hour of the date
+        }
+
+        function getDate(string) {
+          return new Date(string);
+        }
+
+        function IsTimeoverlap() {
+          if (
+            (gethours(getDate(`${data.date}, ${data.start_time}`)) <
+              gethours(
+                getDate(`${data.date}, ${bookings_of_room[i].start_time}`)
+              ) &&
+              gethours(getDate(`${data.date}, ${data.end_time}`)) <
+                gethours(
+                  getDate(`${data.date}, ${bookings_of_room[i].start_time}`)
+                )) ||
+            gethours(getDate(`${data.date}, ${data.start_time}`)) >
+              gethours(
+                getDate(`${data.date}, ${bookings_of_room[i].end_time}`)
+              ) ||
+            gethours(getDate(`${data.date}, ${data.end_time}`)) >
+              gethours(
+                getDate(`${data.date}, ${bookings_of_room[i].end_time} `)
+              )
+          ) {
+            return false;
+          } else {
+            return true;
+          }
+        }
+      }
+    }
+  }
 
   const response = await bookroom(data);
   res.send(response);
 });
 
-app.listen(process.env.PORT, () => console.log(`Server started ${PORT}`));
+//API to list all the rooms with booked data
+//GET : /getallroomsdata
+
+app.get("/getallroomsdata", async (req, res) => {
+  const response = await getAllRoomDataWithBookings();
+  res.send(response);
+});
+
+//API to list all the rooms with booked data
+//GET : /getcustomerbookingdata
+
+app.get("/getcustomerbookingdata", async (req, res) => {
+  const response = await getBookingData();
+  res.send(response);
+});
+
+app.listen(PORT, () => console.log(`Server started ${PORT}`));
 
 //Very important code for mongo connection always just as it is.......
 
